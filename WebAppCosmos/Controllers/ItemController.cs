f2e8b9824198e5be6ffa7850c9e9ccfa73cc3152
maruti <maruti_makwana@hotmail.com>
2020-04-19 15:07:54 +0530

    Add project files.

---
+++ WebAppCosmos/Controllers/ItemController.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.Azure.Documents;
+using Microsoft.Azure.Documents.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+using WebAppCosmos.Models;
+
+namespace WebAppCosmos.Controllers
+{
+    public class ItemController : Controller
+    {
+        // GET: Item
+        [ActionName("Index")]
+        public async Task<ActionResult> IndexAsync()
+        {
+            var items = await DocumentDBRepository<Item>.GetItemsAsync(d => !d.Completed);
+            return View(items);
+        }
+
+        [ActionName("Create")]
+        public async Task<ActionResult> CreateAsync()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ActionName("Create")]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> CreateAsync([Bind(Include = "Id,Name,Description,Completed")] Item item)
+        {
+            if (ModelState.IsValid)
+            {
+                await DocumentDBRepository<Item>.CreateItemAsync(item);
+                return RedirectToAction("Index");
+            }
+
+            return View(item);
+        }
+
+        [HttpPost]
+        [ActionName("Edit")]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> EditAsync([Bind(Include = "Id,Name,Description,Completed")] Item item)
+        {
+            if (ModelState.IsValid)
+            {
+                await DocumentDBRepository<Item>.UpdateItemAsync(item.Id, item);
+                return RedirectToAction("Index");
+            }
+
+            return View(item);
+        }
+
+        [ActionName("Edit")]
+        public async Task<ActionResult> EditAsync(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            Item item = await DocumentDBRepository<Item>.GetItemAsync(id);
+            if (item == null)
+            {
+                return HttpNotFound();
+            }
+
+            return View(item);
+        }
+    }
+}
